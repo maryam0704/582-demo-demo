fd6fc584c96ab4328f3e6b640a3dd08369b01b09
maryam0704 <2195265@edu.vaniercollege.qc.ca>
2023-10-05 20:26:37 -0400

    starting with react

---
+++ src/components/Basic.js
@@ -0,0 +1,14 @@
+function Basic() {
+    return (
+      <>
+        <div>
+          <h1>Basic component</h1>
+            </div>
+            <p>
+                vdgfvdghv dvsvfhsd shdvshzgdh
+            </p>
+
+      </>
+    );
+}
+export default Basic;
